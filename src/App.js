11f6b949383eba04e855d6d8fd91f37155b30655
Tanvir Hussain <107083814+Tanvir-Archfiend-Hussain@users.noreply.github.com>
2022-12-21 23:34:38 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Button from "./components/Button";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Button />
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
