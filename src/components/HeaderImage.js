2ec26d546ed062bb1d8c516f987d18b3545bd846
Tanvir Hussain <tanvirelhussain@gmail.com>
2022-12-21 23:40:52 +0000

    Create HeaderImage.js and yarn.lock

---
+++ src/components/HeaderImage.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+
+const HeaderImage= styled.div`
+        background-image: url(https://thechristmasexperience.co.uk/uploads/2022/08/TCE-SDP4935-570small-13.jpg);
+ ';
+ 
+ export default HeaderImage;
